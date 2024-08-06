bb9a980e7505304db9688922b4a56ba1c4b9a587
gd06070 <72424775+gd06070@users.noreply.github.com>
2024-08-06 10:28:50 +0900

    Create divison_is_zero.js

---
+++ divison_is_zero.js
@@ -0,0 +1,26 @@
+function solution(arr, query) {
+    for (let i = 0; i < query.length; i++) {
+        if (i % 2 === 0) {
+            arr = arr.slice(0, query[i] + 1);
+        } else {
+            arr = arr.slice(query[i]);
+        }
+    }
+    return arr;
+}
+
+/* 오답 코드
+function solution(arr, query) {
+    for (let i = 0; i < query.length; i++) {
+        if (arr.includes(query[i])) {
+            if (i % 2 === 0) {
+                arr.pop()
+            } else {
+                arr.shift()
+            }
+        }
+        
+    }
+    return arr
+} 
+*/
